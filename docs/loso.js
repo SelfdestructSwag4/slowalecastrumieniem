c3427728cbe690c9b0be3c122c4a8ceb157eddd3
SelfdestructSwag4 <Gicikhub@proton.me>
2026-04-02 22:49:48 +0200

    Update loso.js

--- docs/loso.js
+++ docs/loso.js
@@ -1,13 +1,13 @@
 
 export function randomPosition() {
-  const obraz = document.getElementById("img");
-  img.src = "Toyota-Celica-T200VI.jpg";      // 2
-  img.className = "animan";
+  const obraz = document.createElement("img");
+  obraz.src = "Toyota-Celica-T200VI.jpg";      // 2
+  obraz.className = "animan";
 
-  img.style.position = "absolute";
-  img.style.left = Math.random() * window.innerWidth + "px";
-  img.style.top = Math.random() * window.innerHeight + "px";
-  document.body.appendChild(img);
+ obraz.style.position = "absolute";
+  obraz.style.left = Math.random() * window.innerWidth + "px";
+  obraz.style.top = Math.random() * window.innerHeight + "px";
+  document.body.appendChild(obraz);
 }
 
 
