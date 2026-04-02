8475f83116eb786c41f957a886b0688af455c8ff
SelfdestructSwag4 <Gicikhub@proton.me>
2026-04-02 21:34:31 +0200

    Create loso.js

---
+++ docs/loso.js
@@ -0,0 +1,14 @@
+<script>
+function randomPosition() {
+  const img = document.getElementById("car");
+
+  const x = Math.random() * (window.innerWidth - 150);
+  const y = Math.random() * (window.innerHeight - 150);
+
+  img.style.left = x + "px";
+  img.style.top = y + "px";
+}
+
+// zmienia pozycję co 1 sekundę
+setInterval(randomPosition, 1000);
+</script>
